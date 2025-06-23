ebeff7a77ab015930a5593fbdb82dbbcf3c4d068
s-codebase <niranjana4u123@gmail.com>
2025-06-23 15:37:30 +0530

    Expense(Frontend)

---
+++ Dotnet/expense tracher/expense tracher/Controllers/ExpenseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace expense_tracher.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
+
+   
