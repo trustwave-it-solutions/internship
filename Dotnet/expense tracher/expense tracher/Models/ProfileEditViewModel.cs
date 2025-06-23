73321c0dd2376f8596319ba5132fe48dec132f51
fabifathima <ffabi5413@gmail.com>
2025-06-23 15:05:11 +0530

    user authentication

---
+++ Dotnet/expense tracher/expense tracher/Models/ProfileEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace expense_tracher.Models
+{
+    public class ProfileEditViewModel
+    {
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Invalid phone format.")]
+        public string? Phone { get; set; }
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email format.")]
+        public string? Email { get; set; }
+    }
+}
---
+++ Dotnet/expense tracher/expense tracher/Models/RegistrationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace expense_tracher.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Invalid phone format.")]
+        public string? Phone { get; set; }
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email format.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
